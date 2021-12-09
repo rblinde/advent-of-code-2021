89ffa2d5f2d0e3b4b026b4dceb91cc725908f0da
rblinde <robertblinde@gmail.com>
2021-12-09 17:09:40 +0100

    Completed day 1

--- days/day01.js
+++ days/day01.js
@@ -1,9 +1,37 @@
 const partOne = (input) => {
-  return 1;
+  const lines = input.split('\n').map(e => Number(e));
+  let prev = lines[0];
+  let count = 0;
+
+  for (let i = 1; i < lines.length; i++) {
+    const curr = lines[i];
+
+    if (curr > prev) {
+      count++;
+    }
+
+    prev = curr;
+  }
+
+  return count;
 };
 
 const partTwo = (input) => {
-  return 2;
+  const lines = input.split('\n').map(e => Number(e));
+  let prev = lines[0] + lines[1] + lines[2];
+  let count = 0;
+
+  for (let i = 1; i < lines.length - 2; i++) {
+    const curr = lines[i] + lines[i + 1] + lines[i + 2];
+
+    if (curr > prev) {
+      count++;
+    }
+
+    prev = curr;
+  }
+
+  return count;
 };
 
 
