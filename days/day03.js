571cddef66f3fafbd6e723e18d698bcb75db432b
rblinde <robertblinde@gmail.com>
2021-12-09 20:12:05 +0100

    Completed day 3

---
+++ days/day03.js
@@ -0,0 +1,43 @@
+const partOne = (input) => {
+  const lines = input.split('\n');
+  const n = lines.length / 2;
+  const size = lines[0].length;
+  const bits = new Array(size).fill(0);
+
+  for (const line of lines) {
+    for (let i = 0; i < line.length; i++) {
+      bits[i] += parseInt(line[i]);
+    }
+  }
+
+  const gamma = parseInt(bits.map(el => el > n ? '1' : '0').join(''), 2);
+  const epsilon = parseInt(bits.map(el => el > n ? '0' : '1').join(''), 2);
+  return gamma * epsilon;
+};
+
+
+const partTwo = (input) => {
+  const lines = input.split('\n');
+
+  const getBitCount = (numbers, i) => numbers.reduce((curr, next) => curr + parseInt(next[i]), 0);
+
+  const findBit = (numbers, a, b) => {
+    let i = 0;
+
+    while (numbers.length !== 1) {
+      const bitCount = getBitCount(numbers, i);
+      const n = numbers.length / 2;
+      const byte = bitCount >= n ? a : b;
+      numbers = numbers.filter(line => line[i] === byte);
+      i++;
+    }
+
+    return parseInt(numbers[0], 2);
+  };
+
+  const oxygen = findBit([...lines], '1', '0');
+  const co2 = findBit([...lines], '0', '1');
+  return oxygen * co2;
+};
+
+export default { partOne, partTwo };
--- days/index.js
+++ days/index.js
@@ -1,9 +1,11 @@
 import day01 from './day01.js';
 import day02 from './day02.js';
+import day03 from './day03.js';
 
 const days = [
   day01,
   day02,
+  day03,
 ];
 
 export default days;
