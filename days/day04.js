155c0312eac8fe4e47ed4400a32475b5a1302c3b
rblinde <robertblinde@gmail.com>
2021-12-10 15:50:08 +0100

    Completed day 4

---
+++ days/day04.js
@@ -0,0 +1,116 @@
+class Board {
+  constructor() {
+    this.numbers = [];
+  }
+
+
+  add(numbers) {
+    const formatted = numbers.map(num => ({ value: num, checked: false }));
+    this.numbers.push(...formatted)
+  }
+
+
+  draw(number) {
+    const index = this.numbers.findIndex(num => num.value === number);
+
+    if (index !== -1) {
+      this.numbers[index].checked = true;
+    }
+  }
+
+
+  isWinner() {
+    // Horizontal
+    for (let i = 0; i < 5; i++) {
+      const indexes = new Array(5).fill(i * 5).map((val, index) => val + index);
+
+      if (indexes.every(val => this.numbers[val].checked)) {
+        return true;
+      }
+    }
+
+    // Vertical
+    for (let j = 0; j < 5; j++) {
+      const indexes = new Array(5).fill(j).map((val, index) => val + index * 5);
+
+      if (indexes.every(val => this.numbers[val].checked)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+
+  getSumUnchecked() {
+    return this.numbers
+      .filter(num => !num.checked)
+      .map(num => num.value)
+      .reduce((prev, curr) => prev + curr, 0);
+  }
+}
+
+
+const createBoards = (lines) => {
+  const boards = [];
+
+  for (let i = 2; i < lines.length; i += 6) {
+    const board = new Board();
+
+    for (let j = i; j < i + 5; j++) {
+      const boardNumbers = lines[j].match(/\d+/g).map(e => Number(e));
+      board.add(boardNumbers);
+    }
+
+    boards.push(board);
+  }
+
+  return boards;
+};
+
+
+const partOne = (input) => {
+  const lines = input.split('\n');
+  const numbers = lines[0].split(',').map(e => Number(e));
+  const boards = createBoards(lines);
+
+  // Draw numbers and pick winner
+  for (const number of numbers) {
+    for (const board of boards) {
+      board.draw(number);
+
+      if (board.isWinner()) {
+        return board.getSumUnchecked() * number;
+      }
+    }
+  }
+
+  return -1;
+};
+
+
+const partTwo = (input) => {
+  const lines = input.split('\n');
+  const numbers = lines[0].split(',').map(e => Number(e));
+  let boards = createBoards(lines);
+
+  // Draw numbers and pick winner
+  for (const number of numbers) {
+    for (const board of boards) {
+      board.draw(number);
+
+      if (board.isWinner()) {
+        if (boards.length === 1) {
+          return board.getSumUnchecked() * number;
+        } else {
+          boards = boards.filter(b => b !== board);
+        }
+      }
+    }
+  }
+
+  return -1;
+};
+
+
+export default { partOne, partTwo };
--- days/index.js
+++ days/index.js
@@ -1,11 +1,13 @@
 import day01 from './day01.js';
 import day02 from './day02.js';
 import day03 from './day03.js';
+import day04 from './day04.js';
 
 const days = [
   day01,
   day02,
   day03,
+  day04,
 ];
 
 export default days;
